70beb274dcca58de0404418f9beb0f42677ae120
Patrick G <gscience213@gmail.com>
2022-03-03 15:55:03 +0000

    Assignment 2 Finished

---
+++ Ass_2/p7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Two mandolins like creatures in the dark" << endl << endl;
+    cout << "Creating the agony of ecstasy" << endl << endl;
+    cout << "                       - George Baker" << endl;
+} 
